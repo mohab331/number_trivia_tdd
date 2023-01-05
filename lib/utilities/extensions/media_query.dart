802271a2cface9816e28fcbae801e99d17d203be
mohab331 <www.mohabosama787@gmail.com>
2023-01-05 13:22:05 +0200

    feat(utilities): Added mediaQuery extension to get sizes

---
+++ lib/utilities/extensions/media_query.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+extension MediaQueryValues on BuildContext {
+  double get width => MediaQuery.of(this).size.width;
+
+  double get height => MediaQuery.of(this).size.height;
+
+  double get top => MediaQuery.of(this).padding.top;
+
+  double get bottom => MediaQuery.of(this).viewInsets.bottom;
+}
