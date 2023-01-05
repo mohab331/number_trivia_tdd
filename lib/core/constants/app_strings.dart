dab42504ecaa0b3620b6a2101e4924d835907e73
mohab331 <www.mohabosama787@gmail.com>
2023-01-05 13:24:51 +0200

    feat(core): Added constants to handle constants app_strings and etc.

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,16 @@
+class AppStrings {
+  // app title
+  static const String appTitle = 'TDD and Clean arch app';
+
+  // home screen
+  static const String homeScreenAppBarTitle = 'Number Trivia';
+
+  //home content
+  static const String startSearching = 'Start Searching!';
+  static const String numberLabel = 'Enter number';
+  static const String searchButtonLabel = 'Search';
+  static const String getRandomTriviaButtonLabel = 'Get Random Trivia';
+
+  // error messages
+  static const String errorGettingData = 'Error while getting data';
+}
